b70e43533de42a2e80bc98c228477a9b94f5b083
noajshu <noahshutty@gmail.com>
2016-02-22 16:45:37 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,16 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+with open('./requirements.txt') as reqs_txt:
+    requirements = [line for line in reqs_txt]
+
+
 setup(
     name='sdpm',
     version='0.0.1',
     description='Simple Docker Python Module',
     long_description=readme,
+    install_requires=requirements,
     author='Noah Shutty',
     author_email='noah@deepgram.com',
     url='https://github.com/noajshu/sdpm',
